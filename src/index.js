fb7aa0dff992898f3da9cca847f8d75b7cb43007
beomseok.choi <cbs412@naver.com>
2022-03-01 13:57:32 +0900

    map state/dispatch to props

--- src/index.js
+++ src/index.js
@@ -1,71 +1,12 @@
-import { createStore } from "redux";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import { Provider } from "react-redux";
+import store from "./store";
 
-const form = document.querySelector("form");
-const input = document.querySelector("input");
-const ul = document.querySelector("ul");
-
-const ADD_TODO = "ADD_TODO";
-const DELETE_TODO = "DELETE_TODO";
-
-const addToDo = (text) => {
-  return {
-    type: ADD_TODO,
-    text,
-    id: Date.now(),
-  };
-};
-
-const deleteToDo = (id) => {
-  return { type: DELETE_TODO, id };
-};
-
-const reducer = (state = [], action) => {
-  switch (action.type) {
-    case ADD_TODO:
-      return [{ text: action.text, id: action.id }, ...state]; // Never mutating state!, Naver use Date.now() in reducer! check the doc!
-    case DELETE_TODO:
-      console.log(action.id);
-      return state.filter((toDo) => toDo.id !== action.id);
-    default:
-      return state;
-  }
-};
-
-const store = createStore(reducer);
-
-store.subscribe(() => console.log(store.getState()));
-
-const dispatchAddTodo = (text) => {
-  store.dispatch(addToDo(text));
-};
-
-const dispatchDeleteTodo = (event) => {
-  const id = parseInt(event.target.parentNode.id);
-  store.dispatch(deleteToDo(id));
-};
-
-const paintTodos = () => {
-  const toDos = store.getState();
-  ul.innerHTML = "";
-  toDos.forEach((toDo) => {
-    const li = document.createElement("li");
-    const btn = document.createElement("button");
-    btn.innerText = "delete";
-    btn.addEventListener("click", dispatchDeleteTodo);
-    li.id = toDo.id;
-    li.innerText = toDo.text;
-    li.appendChild(btn);
-    ul.appendChild(li);
-  });
-};
-
-store.subscribe(paintTodos);
-
-const onSubmit = (event) => {
-  event.preventDefault();
-  const toDo = input.value;
-  input.value = "";
-  dispatchAddTodo(toDo);
-};
-
-form.addEventListener("submit", onSubmit);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
